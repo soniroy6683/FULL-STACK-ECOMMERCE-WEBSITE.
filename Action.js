78e1c7287b7018b723d71c569b805f2534998113
Soni roy <165287536+soniroy6683@users.noreply.github.com>
2024-04-22 21:03:15 +0530

    Add files via upload

--- Action.js
+++ Action.js
@@ -1,114 +1,65 @@
-import {
-  getUserById,
-  updatePersonalInformationFetch,
-  getOrderByUser,
-  updatePassword,
-} from "./FetchApi";
+import React from "react";
+import { postAddReview, postDeleteReview } from "./FetchApi";
+import { isAuthenticate } from "../auth/fetchApi";
 
-export const logout = () => {
-  localStorage.removeItem("jwt");
-  localStorage.removeItem("cart");
-  localStorage.removeItem("wishList");
-  window.location.href = "/";
-};
+export const Alert = (color, text) => (
+  <div className={`bg-${color}-200 px-4 py-2 my-2 rounded`}>{text}</div>
+);
 
-export const fetchData = async (dispatch) => {
-  dispatch({ type: "loading", payload: true });
-  let userId = JSON.parse(localStorage.getItem("jwt"))
-    ? JSON.parse(localStorage.getItem("jwt")).user._id
-    : "";
-  try {
-    let responseData = await getUserById(userId);
-    setTimeout(() => {
-      if (responseData && responseData.User) {
-        dispatch({ type: "userDetails", payload: responseData.User });
-        dispatch({ type: "loading", payload: false });
-      }
-    }, 500);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const fetchOrderByUser = async (dispatch) => {
-  dispatch({ type: "loading", payload: true });
-  let userId = JSON.parse(localStorage.getItem("jwt"))
-    ? JSON.parse(localStorage.getItem("jwt")).user._id
-    : "";
-  try {
-    let responseData = await getOrderByUser(userId);
-    setTimeout(() => {
-      if (responseData && responseData.Order) {
-        console.log(responseData);
-        dispatch({ type: "OrderByUser", payload: responseData.Order });
-        dispatch({ type: "loading", payload: false });
-      }
-    }, 500);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updatePersonalInformationAction = async (dispatch, fData) => {
-  const formData = {
-    uId: fData.id,
-    name: fData.name,
-    phoneNumber: fData.phone,
-  };
-  dispatch({ type: "loading", payload: true });
-  try {
-    let responseData = await updatePersonalInformationFetch(formData);
-    setTimeout(() => {
-      if (responseData && responseData.success) {
-        dispatch({ type: "loading", payload: false });
-        fetchData(dispatch);
-      }
-    }, 500);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const handleChangePassword = async (fData, setFdata, dispatch) => {
-  if (!fData.newPassword || !fData.oldPassword || !fData.confirmPassword) {
-    setFdata({
-      ...fData,
-      error: "Please provide your all password and a new password",
-    });
-  } else if (fData.newPassword !== fData.confirmPassword) {
-    setFdata({ ...fData, error: "Password does't match" });
+export const reviewSubmitHanlder = (fData, setFdata, fetchData) => {
+  if (!fData.rating || !fData.review) {
+    setFdata({ ...fData, error: "Rating and review must be required" });
+  } else if (!isAuthenticate()) {
+    setFdata({ ...fData, error: "You must need login to review" });
   } else {
-    const formData = {
-      uId: JSON.parse(localStorage.getItem("jwt")).user._id,
-      oldPassword: fData.oldPassword,
-      newPassword: fData.newPassword,
-    };
-    dispatch({ type: "loading", payload: true });
-    try {
-      let responseData = await updatePassword(formData);
-      if (responseData && responseData.success) {
-        setFdata({
-          ...fData,
-          success: responseData.success,
-          error: "",
-          oldPassword: "",
-          newPassword: "",
-          confirmPassword: "",
-        });
-        dispatch({ type: "loading", payload: false });
-      } else if (responseData.error) {
-        dispatch({ type: "loading", payload: false });
-        setFdata({
-          ...fData,
-          error: responseData.error,
-          success: "",
-          oldPassword: "",
-          newPassword: "",
-          confirmPassword: "",
-        });
-      }
-    } catch (error) {
-      console.log(error);
+    addReview(fData, setFdata, fetchData);
+  }
+};
+
+export const deleteReview = async (
+  reviewId,
+  productId,
+  fetchData,
+  setFdata
+) => {
+  try {
+    let responseData = await postDeleteReview({
+      rId: reviewId,
+      pId: productId,
+    });
+    if (responseData.success) {
+      fetchData();
+      setFdata({ success: responseData.success });
+    } else if (responseData.error) {
+      fetchData();
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addReview = async (fData, setFdata, fetchData) => {
+  let formData = {
+    rating: fData.rating,
+    review: fData.review,
+    pId: fData.pId,
+    uId: JSON.parse(localStorage.getItem("jwt")).user._id,
+  };
+  try {
+    let responseData = await postAddReview(formData);
+    if (responseData.success) {
+      setFdata({
+        ...fData,
+        success: responseData.success,
+        review: "",
+        rating: "",
+      });
+      fetchData();
+    } else if (responseData.error) {
+      setFdata({ ...fData, error: responseData.error, review: "", rating: "" });
+      fetchData();
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
--- FetchApi.js
+++ FetchApi.js
@@ -1,36 +1,12 @@
 import axios from "axios";
 const apiURL = process.env.REACT_APP_API_URL;
 
-export const getUserById = async (uId) => {
+export const wishListProducts = async () => {
+  let productArray = JSON.parse(localStorage.getItem("wishList"));
   try {
-    let res = await axios.post(`${apiURL}/api/user/signle-user`, { uId });
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updatePersonalInformationFetch = async (userData) => {
-  try {
-    let res = await axios.post(`${apiURL}/api/user/edit-user`, userData);
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getOrderByUser = async (uId) => {
-  try {
-    let res = await axios.post(`${apiURL}/api/order/order-by-user`, { uId });
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updatePassword = async (formData) => {
-  try {
-    let res = await axios.post(`${apiURL}/api/user/change-password`, formData);
+    let res = await axios.post(`${apiURL}/api/product/wish-product`, {
+      productArray,
+    });
     return res.data;
   } catch (error) {
     console.log(error);
